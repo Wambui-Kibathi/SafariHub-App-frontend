a640dd15ac0357b2a6866a9660e5f1f2e40a94f7
Tembea360 <tembea360@example.com>
2025-11-03 12:26:23 +0300

    SAF-40 created a profile page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { FaUserPlus, FaEye, FaEyeSlash, FaSignInAlt } from "react-icons/fa";
-import "../styles/Form.css";
+import "../styles/form.css";
 import "../styles/main.css";
 
 const Register = () => {
